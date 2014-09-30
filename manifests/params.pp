11fbf9a5554d99a4983b48dc47c6d40addde817d
Eric Shamow <eric.shamow@gmail.com>
2014-09-30 14:37:47 -0700

    Parameterize group name for web service

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,13 @@
 class puppetmaster::params {
   $user = 'puppet'
   $group = 'puppet'
-  $web_group = 'apache'
   $puppet_root = '/etc/puppet'
   $puppet_usr_root = '/usr/share/puppet'
   $puppet_lib_root = '/var/lib/puppet'
+
+  case $::osfamily {
+    'redhat': { $web_group = 'apache' }
+    'debian': { $web_group = 'www-data' }
+    default:  { fail("osfamily ${::osfamily} is unsupported by class puppetmaster.") }
+  }
 }
