88c0ee78f215389766ee19011ca2a004da5c845c
Eric Shamow <eric.shamow@gmail.com>
2014-10-01 20:33:06 -0700

    Modify inheritence structure

--- manifests/profile.pp
+++ manifests/profile.pp
@@ -4,7 +4,7 @@ class puppetmaster::profile(
   $manage_firewall  = true,
   $firewall_service = $puppetmaster::params::firewall_service,
   $r10k_version     = 'installed'
-) inherits puppetmaster::params {
+) inherits puppetmaster {
   if ($master == undef) or ($control_repo == undef) {
     fail('$master and $control_repo must be defined for profile puppetmaster.')
   }
--- manifests/profile/puppetboard.pp
+++ manifests/profile/puppetboard.pp
@@ -1,7 +1,10 @@
-class puppetmaster::profile::puppetboard {
+class puppetmaster::profile::puppetboard (
+  $master = puppetmaster::master
+) inherits puppetmaster::profile {
   class { 'apache::mod::wsgi': }
   class { 'puppetboard': }
   class { 'puppetboard::apache::vhost':
+    name => $master,
     port => 80,
   }
 }
