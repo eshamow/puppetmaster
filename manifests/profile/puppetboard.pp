5c4a50fbd039c5424b87d178e25e146b0bdff7bb
Eric Shamow <eric.shamow@gmail.com>
2014-10-03 22:53:01 -0700

    Have puppetboard install virtualenv

--- manifests/profile/puppetboard.pp
+++ manifests/profile/puppetboard.pp
@@ -2,7 +2,9 @@ class puppetmaster::profile::puppetboard (
   $master = puppetmaster::master
 ) inherits puppetmaster::params {
   class { 'apache::mod::wsgi': }
-  class { '::puppetboard': }
+  class { '::puppetboard':
+    manage_virtualenv => true,
+  }
   class { 'puppetboard::apache::vhost':
     vhost_name => $master,
     port => 80,
