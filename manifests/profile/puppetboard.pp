3ebdf24c2e8cb16292e30595ac12368b2ef60689
Eric Shamow <eric.shamow@gmail.com>
2014-10-03 22:34:23 -0700

    Class namespace resolution

--- manifests/profile/puppetboard.pp
+++ manifests/profile/puppetboard.pp
@@ -2,7 +2,7 @@ class puppetmaster::profile::puppetboard (
   $master = puppetmaster::master
 ) inherits puppetmaster::params {
   class { 'apache::mod::wsgi': }
-  class { 'puppetboard': }
+  class { '::puppetboard': }
   class { 'puppetboard::apache::vhost':
     name => $master,
     port => 80,
