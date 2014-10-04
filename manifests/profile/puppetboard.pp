2331dfa247626ae57f1d1f6ecee54e62214cc6be
Eric Shamow <eric.shamow@gmail.com>
2014-10-03 23:08:12 -0700

    Move virtualenv management to python class

--- manifests/profile/puppetboard.pp
+++ manifests/profile/puppetboard.pp
@@ -2,8 +2,12 @@ class puppetmaster::profile::puppetboard (
   $master = puppetmaster::master
 ) inherits puppetmaster::params {
   class { 'apache::mod::wsgi': }
-  class { 'python': } ->
-  class { '::puppetboard': }
+  class { 'python':
+    virtualenv => true,
+  } ->
+  class { '::puppetboard':
+    manage_virtualenv => false,
+  }
   class { 'puppetboard::apache::vhost':
     vhost_name => $master,
     port => 80,
