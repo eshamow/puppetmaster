f21bc1bdba2b9c50c6eab5e6e2064bc5a4aa8b9b
Eric Shamow <eric.shamow@gmail.com>
2014-10-05 12:22:31 -0700

    Turn Puppetboard SSL back on

--- manifests/profile/puppetboard.pp
+++ manifests/profile/puppetboard.pp
@@ -12,7 +12,7 @@ class puppetmaster::profile::puppetboard (
     puppetdb_host => $master,
     puppetdb_port => '8081',
     puppetdb_key  => '/etc/puppetdb/ssl/private.pem',
-    puppetdb_ssl  => 'False',
+    puppetdb_ssl  => '/etc/puppetdb/ssl/ca.pem',
     puppetdb_cert => '/etc/puppetdb/ssl/public.pem',
   }
   class { 'puppetboard::apache::conf': }
