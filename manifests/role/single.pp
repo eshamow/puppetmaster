ccf4e967af15853fd7239c7f7b1663c33a4397e9
Eric Shamow <eric.shamow@gmail.com>
2014-10-19 14:49:11 -0700

    Test adding back puppetboard

--- manifests/role/single.pp
+++ manifests/role/single.pp
@@ -1,6 +1,6 @@
 # NOTE: Must call this with profile parameters passed via data bindings
 class puppetmaster::role::single {
   class { 'puppetmaster::profile': }
-  #  class { 'puppetmaster::profile::puppetboard': }
+  class { 'puppetmaster::profile::puppetboard': }
   #  class { 'puppetmaster::profile::puppetexplorer': }
 }
